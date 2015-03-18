8223a053c2153793d0fd56051eee7e039ec393f2
Mark Yen <mark@uber.com>
2015-03-17 18:39:24 -0700

    Use a TypedArray instead of a number

--- bit-ring.js
+++ bit-ring.js
@@ -1,9 +1,6 @@
-var assert = require('assert');
-
 function BitRing(capacity) {
-    assert.ok(capacity < 32, 'Capacity must be less than 32');
     this.capacity = capacity;
-    this.bits = 0;
+    this.bits = new Uint8ClampedArray(capacity);
     this.pos = 0;
     this.length = 0;
     this._count = 0;
@@ -12,8 +9,8 @@ function BitRing(capacity) {
 // Update the count and set or clear the next bit in the ring
 BitRing.prototype.push = function(bool) {
     var num = bool === true ? 1 : 0;
-    this._count += num - ((this.bits >>> this.pos) & 1);
-    this.bits ^= (-num ^ this.bits) & (1 << this.pos);
+    this._count += num - this.bits[this.pos];
+    this.bits[this.pos] = num;
     this.pos = (this.pos + 1) % this.capacity;
     if (this.length < this.capacity) {
         this.length++;
